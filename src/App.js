039f728fd4ff6fe73112b18624f66a4574b60add
PaulTA91 <parmst202@caledonian.ac.uk>
2022-11-18 15:31:20 +0000

    Lab 7, aboutus not working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+import Locations from "./components/Locations";
+import Navigation from "./components/Navigation";
+import Person from "./components/Person";
+import { people } from "./data/data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route path="/" element={<Home />} />
+          <Route path="aboutus" element={<AboutUs />}>
+            <Route path=":personId" element={<Person people={people} />} />
+          </Route>
+          <Route path="locations" element={<Locations />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ src/components/Person.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Person = ({ people }) => {
+  const { personId } = useParams();
+  const currentItem = people.filter((entry) => {
+    return entry.id === personId;
+  });
+  const { fullName, bio } = currentItem[0];
+
+  return (
+    <>
+      <h4>Staff Member: {fullName}</h4>
+      <p>{bio}</p>
+    </>
+  );
+};
+
+export default Person;
